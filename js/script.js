11b545f8786abc5715915119ace529f3eaa8a8a7
Mukhtar Mahdi <mukhtar.mahdi217@gmail.com>
2018-12-21 05:34:08 +0000

    N2O Complete

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// JavaScript File
+/* global $ */
+$(".pro").click(function(){
+    $(".header").css("border-color","white");
+    $("#img").attr("src","https://ak9.picdn.net/shutterstock/videos/28955929/thumb/10.jpg");
+    $("#text").html("N2O is a gas in room temperature. It does not conduct electricity. It is barely soluble. It has a low melting and boiling points.");
+    $(".buttons").css("font-weight","none");
+    $(".buttons").css("color","white");
+    $(".pro").css("font-weight","bold");
+    $(".pro").css("color","#F64C72");
+});
+
+$(".comp").click(function(){
+    $(".header").css("border-color","white");
+        $("#img").attr("src","https://previews.123rf.com/images/molekuul/molekuul1709/molekuul170900310/85870855-nitrous-oxide-nos-laughing-gas-n2o-molecule-used-in-surgery-as-analgesic-and-anesthetic-drug-and-als.jpg");
+    $("#text").html("N2O consists of 2 Nitrogen atoms and one Oxygen atom. Both of these atoms are Non-Metals, therefore N2O is a Covalent Compound.");
+        $(".buttons").css("font-weight","none");
+    $(".buttons").css("color","white");
+    $(".comp").css("font-weight","bold");
+    $(".comp").css("color","#F64C72");
+});
+
+$(".ic").click(function(){
+    $(".header").css("border-color","white");
+    $("#img").attr("src","https://thisonevsthatone.com/wp-content/uploads/Ionic-vs-Covalent.png");
+    $("#text").html("N2O is a Covalent bond because they both have low melting and boiling points, no coductivity, and All N2O's elements are Non-Metals."); 
+        $(".buttons").css("font-weight","none");
+    $(".buttons").css("color","white");
+    $(".ic").css("font-weight","bold");
+    $(".ic").css("color","#F64C72");
+});
+
+$(".wu").click(function(){
+    $(".header").css("border-color","white");
+    $("#img").attr("src","https://news.vanderbilt.edu/files/laughing-gas.jpg");
+    $("#text").html("N2O is used for sedation and pain relief, but is also used by people to feel intoxicated or high. It is commonly used by dentists and medical professionals to sedate patients undergoing minor medical procedures.");   
+        $(".buttons").css("font-weight","none");
+    $(".buttons").css("color","white");
+    $(".wu").css("font-weight","bold");
+    $(".wu").css("color","#F64C72");
+});
+
+$(".header").click(function(){
+    $("#img").attr("src","https://previews.123rf.com/images/mariashmitt/mariashmitt1512/mariashmitt151200049/50004831-n2o-nitrous-oxide-3d-molecule-isolated-on-white.jpg");
+    $("#text").html("N2O, is a colorless, almost odorless gas, that was first discovered in 1772 by the English scientist and clergyman Joseph Priestley.  Priestley made N2O by heating ammonium nitrate in the presence of iron filings, and then passing the gas that came off (NO) through water to remove toxic by-products.");    
+            $(".buttons").css("font-weight","none");
+    $(".buttons").css("color","white");
+    $(".header").css("border-color","#F64C72");
+});
---
+++ script.js
@@ -0,0 +1,27 @@
+// JavaScript File
+/* global $ */
+$(".pro").click(function(){
+    $("#img").attr("src","");
+    $("#text").html("");
+    alert("Hi");
+});
+
+$(".comp").click(function(){
+        $("#img").attr("src","");
+    $("#text").html("");
+});
+
+$(".ic").click(function(){
+    $("#img").attr("src","");
+    $("#text").html("");    
+});
+
+$(".wu").click(function(){
+    $("#img").attr("src","");
+    $("#text").html("");    
+});
+
+$(".header").click(function(){
+    $("#img").attr("src","https://previews.123rf.com/images/mariashmitt/mariashmitt1512/mariashmitt151200049/50004831-n2o-nitrous-oxide-3d-molecule-isolated-on-white.jpg");
+    $("#text").html("N2O, is a colorless, almost odorless gas, that was first discovered in 1772 by the English scientist and clergyman Joseph Priestley.  Priestley made N2O by heating ammonium nitrate in the presence of iron filings, and then passing the gas that came off (NO) through water to remove toxic by-products.");    
+});
